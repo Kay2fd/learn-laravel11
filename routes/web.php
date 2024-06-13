5aae10c9ade00da355cf66272646df492fd9303d
Dika Pangestu <dikdika2406@gmail.com>
2024-06-13 20:17:30 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -12,11 +14,28 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+    // eager loading
+    //$posts = Post::with(['author', 'category'])->latest()->get();
+
+    $posts = Post::all();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/posts/{post:slug}', function(Post $post) {
         return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user:username}', function(User $user) {
+    // lazy eager loading
+   // $posts = $user->posts->load('category', 'author');
+
+    return view('posts', ['title' => count($user->posts) . ' Articles By '. $user->name, 'posts' => $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function(Category $category) {
+   // $posts = $category->posts->load('category', 'author');
+
+    return view('posts', ['title' => 'Articles in: ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
